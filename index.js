3fb5fa4f04b5739af20f47467e2a4a7d16b404cc
Anish Chaudhary <chaudharyanish305@gmail.com>
2022-10-09 21:06:55 +0545

    Improvement: Learned about node events

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-const  {format} = require('date-fns');
+const logEvents = require('./logEvents');
+const EventEmitter = require('events');
 
-console.log(format(new Date(), 'yyyy-MM-dd\tHH:mm:ss'));
+class MyEmitter extends EventEmitter{ };
+
+//initialize object
+const myEmitter = new MyEmitter();
+
+//add listener for the log event
+myEmitter.on('log', (msg)=> logEvents(msg));
+
+setTimeout(()=>{
+    //Emit event
+    myEmitter.emit('log', 'log event emitted!')
+})
