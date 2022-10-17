8c905969d32f113bee57f73396b66062a2212390
Anish Chaudhary <chaudharyanish305@gmail.com>
2022-10-17 16:37:14 +0545

    Improvement: Route in express

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 3500;
 const { logger } = require(path.join(__dirname, "middleware", "logEvents.js"));
-const errotHandler   = require(path.join(
+const errotHandler = require(path.join(
   __dirname,
   "middleware",
   "errorHandler.js"
@@ -43,58 +43,52 @@ app.use(express.json());
 app.use(cors());
 
 //serve static files
-app.use(express.static(path.join(__dirname, "/public")));
+app.use("/", express.static(path.join(__dirname, "/public")));
+app.use("/subdir", express.static(path.join(__dirname, "/public")));
 
-app.get("^/$|/index(.html)?", (req, res) => {
-  //   res.send('home page');
-  //  res.sendFile('./views/index.html', { root:__dirname});
-  res.sendFile(path.join(__dirname, "views", "index.html"));
-});
-
-app.get("/new-page(.html)?", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "new-page.html"));
-});
-
-app.get("/old-page(.html)?", (req, res) => {
-  res.redirect(301, "new-page.html");
-});
+//using routes from another files
+app.use("/", require("./routes/root"));
+app.use("/subdir", require("./routes/subdir"));
+//routs-for api
+app.use("/employees", require('./routes/api/employees'))
+ 
 
 //Route Handlers
-app.get(
-  "/hello(.html)?",
-  (req, res, next) => {
-    console.log("attempted to load hello.html");
-    next();
-  },
-  (req, res) => {
-    res.send("hello world");
-  }
-);
+// app.get(
+//   "/hello(.html)?",
+//   (req, res, next) => {
+//     console.log("attempted to load hello.html");
+//     next();
+//   },
+//   (req, res) => {
+//     res.send("hello world");
+//   }
+// );
 
 //chaining route handlers
-const one = (req, res, next) => {
-  console.log("one");
-  next();
-};
-const two = (req, res, next) => {
-  console.log("two");
-  next();
-};
-const three = (req, res) => {
-  console.log("three");
-  res.send("finished");
-};
+// const one = (req, res, next) => {
+//   console.log("one");
+//   next();
+// };
+// const two = (req, res, next) => {
+//   console.log("two");
+//   next();
+// };
+// const three = (req, res) => {
+//   console.log("three");
+//   res.send("finished");
+// };
 
-app.get("/chain(.html)?", [one, two, three]);
+// app.get("/chain(.html)?", [one, two, three]);
 
 app.all("*", (req, res) => {
   res.status(404);
-  if(req.accepts('html')){
+  if (req.accepts("html")) {
     res.sendFile(path.join(__dirname, "views", "404.html"));
-  }else if(req.accepts('json')){
-    res.json({error:"404 Not Found"});
-  }else{
-    res.type('txt').send("404 Not Found");
+  } else if (req.accepts("json")) {
+    res.json({ error: "404 Not Found" });
+  } else {
+    res.type("txt").send("404 Not Found");
   }
 });
 
