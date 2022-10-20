3a796830c4fe843fca3ebd2a88dde139f1b749a8
Anish Chaudhary <chaudharyanish305@gmail.com>
2022-10-20 15:15:46 +0545

    BugFix: Cors error fixed

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 // cross origin Resource Sharing -third party middleware
-app.use(cors());
+// app.use(cors());
 
 //serve static files
 app.use("/", express.static(path.join(__dirname, "/public")));
